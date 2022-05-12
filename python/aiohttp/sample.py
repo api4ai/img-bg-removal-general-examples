e85bd62df8f6c9ba962262afd795c8400e10e656
Vladislav Gubarev <gubarev@api4.ai>
2022-05-12 15:06:04 +0300

    Python/aiohttp: Removed minor comment.

--- python/aiohttp/sample.py
+++ python/aiohttp/sample.py
@@ -41,7 +41,6 @@ async def main():
     """Entry point."""
     image = sys.argv[1] if len(sys.argv) > 1 else 'https://storage.googleapis.com/api4ai-static/samples/img-bg-removal-3.jpg'
 
-    # response = None
     async with aiohttp.ClientSession() as session:
         if '://' in image:
             # Data from image URL.
